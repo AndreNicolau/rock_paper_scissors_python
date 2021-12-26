f64973e4f187a8d50b81b642eae743561265cf32
rodinhaass <andre.filipe.nicolau29@gmail.com>
2021-12-26 05:57:50 +0000

    Storing data in json file

--- main_module.py
+++ main_module.py
@@ -1,8 +1,9 @@
 import random
 import os
+import json
 
 __LIST__ = ['ROCK', 'PAPER', 'SCISSORS']  # list of playable options
-comp_play = usr_play = ''  # variables to computer and user playable options
+comp_play = usr_play = winner = ''  # variables to computer and user playable options
 
 
 # define global variable for computer play
@@ -23,34 +24,56 @@ def user_play():
 
 # check who wins
 def check_winner():
+    global winner
     if comp_play == 'ROCK' and usr_play == 'PAPER':
         print('User wins!')
+        winner = 'User'
     if comp_play == 'ROCK' and usr_play == 'SCISSORS':
         print('Computer wins!')
+        winner = 'Computer'
     if comp_play == 'PAPER' and usr_play == 'SCISSORS':
         print('User wins!')
+        winner = 'User'
     if comp_play == 'PAPER' and usr_play == 'ROCK':
         print('Computer wins!')
+        winner = 'Computer'
     if comp_play == 'SCISSORS' and usr_play == 'ROCK':
         print('User wins!')
+        winner = 'User'
     if comp_play == 'SCISSORS' and usr_play == 'PAPER':
         print('Computer wins!')
+        winner = 'Computer'
     elif comp_play == usr_play:
         print('It`s a tie!')
+        winner = 'None'
+    return winner
 
 
 # print what computer played
 # print what player played
 def print_game():
+    global usr_play, comp_play
     print(f'\nComputer played: {comp_play}')
     print(f'User played: {usr_play}')
+    return comp_play, usr_play
 
+def store_data():
+    dictionary = {
+        "Winner": f"{winner}",
+        "Computer played: ": f"{comp_play}",
+        "Player played": f"{usr_play}"
+    }
+
+    with open('data.json', 'w') as write_file:
+        json.dump(dictionary, write_file, indent=4)
+        
 
 def main():
     computer_play()
     user_play()
     check_winner()
     print_game()
+    store_data()
 
 
 if __name__ == '__main__':
